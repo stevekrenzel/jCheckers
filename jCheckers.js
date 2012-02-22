963d445e0d5cc724e8be66230723d4b7604ea220
Zack Parker <zack@zackparker.org>
2012-02-21 23:28:44 -0800

    Sweet, an initial board layout.

--- jCheckers.js
+++ jCheckers.js
@@ -1,5 +1,34 @@
 var square = function (spec) {
 	var that = {};
+	var piece;
+
+	that.getColor = function ( ) {
+		return spec.color;
+	}
+
+	that.setPiece = function (newPiece) {
+		piece = newPiece;
+	}
+
+	that.getPiece = function ( ) {
+		return piece || undefined;
+	}
+
+	return that;
+}
+
+var piece = function (spec) {
+	var that = {};
+
+	that.getPlayer = function ( ) {
+		return spec.player;
+	}
+
+	return that;
+}
+
+var player = function (spec) {
+	var that = {};
 
 	that.getColor = function ( ) {
 		return spec.color;
@@ -8,10 +37,17 @@ var square = function (spec) {
 	return that;
 }
 
+function reverseColor(thisColor) {
+	if (thisColor == 'light') {
+		return 'dark';
+	} else {
+		return 'light';
+	}
+}
+
+// Initialize the board.
 var board = { };
-
 board.rows = [ ];
-
 var thisColor = 'light';
 for (i=0; i<8; i++) {
 	thisColor = reverseColor(thisColor);
@@ -22,10 +58,21 @@ for (i=0; i<8; i++) {
 	}
 }
 
-function reverseColor(thisColor) {
-	if (thisColor == 'light') {
-		return 'dark';
-	} else {
-		return 'light';
+// Initialize the players.
+var players = [ player({color: 'black'}), player({color: 'red'}) ];
+
+// Initialize the pieces. (I'll clean this up later.)
+for (i=0; i<3; i++) {
+	for (ii=0; ii<board.rows[i].length; ii++) {
+		if (board.rows[i][ii].getColor() == 'dark') {
+			board.rows[i][ii].setPiece( piece({player: players[0]}) );
+		}
+	}
+}
+for (i = (board.rows.length - 1); i > (board.rows.length - 4); i--) {
+	for (ii=0; ii<board.rows[i].length; ii++) {
+		if (board.rows[i][ii].getColor() == 'dark') {
+			board.rows[i][ii].setPiece( piece({player: players[1]}) );
+		}
 	}
 }
