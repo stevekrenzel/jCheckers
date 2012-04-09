2f25a2d8bc3332453127c61b3bd05d51828f5aa2
Zack Parker <zack@zackparker.org>
2012-04-08 19:32:25 -0700

    HOLY SHIT THE FIRST TURN CAN BE PLAYED

--- jCheckers.js
+++ jCheckers.js
@@ -23,6 +23,27 @@ var player = {
 	}
 }
 
+var square = {
+	htmlId: function () {
+		return "square-" + this.yCoordinate + "-" + this.xCoordinate;
+	},
+
+	canMoveTo: function (otherSquare) {
+		if (otherSquare.color == 'light') {
+			return false;
+		}
+		if (typeof otherSquare.piece !== 'undefined') {
+			return false;
+		}
+		if (Math.abs(otherSquare.xCoordinate - this.xCoordinate) == 1) {
+			if (Math.abs(otherSquare.yCoordinate - this.yCoordinate) == 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
 // Initialize the players.
 var players = [ Object.create(player), Object.create(player) ];
 players[0].color = 'black';
@@ -31,6 +52,16 @@ players[1].color = 'red';
 var turn = {
 	draw: function () {
 		return "<div id=\"notice\">" + this.player.color + " player's move.</div>";
+	},
+
+	selectSquare: function (newSquare) {
+		if (typeof this.selectedSquare !== 'undefined') {
+			$("#" + this.selectedSquare.htmlId()).addClass('dark');
+			$("#" + this.selectedSquare.htmlId()).removeClass('selected');
+		}
+		this.selectedSquare = newSquare;
+		$("#" + newSquare.htmlId()).removeClass('dark');
+		$("#" + newSquare.htmlId()).addClass('selected');
 	}
 }
 
@@ -38,22 +69,24 @@ var turn = {
 var board = { };
 var thisColor = 'light';
 board.rows = [ ];
-_.times(8, function(i) {
+_.times(8, function (i) {
 	thisColor = reverseColor(thisColor);
 	board.rows[i] = [ ];
-	_.times(8, function(ii) {
+	_.times(8, function (ii) {
 		thisColor = reverseColor(thisColor);
-		board.rows[i][ii] = Object.create({});
+		board.rows[i][ii] = Object.create(square);
 		board.rows[i][ii].color = thisColor;
+		board.rows[i][ii].xCoordinate = ii;
+		board.rows[i][ii].yCoordinate =  i;
 	});
 });
 
-board.draw = function() {
+board.draw = function () {
 	var boardElement = "<table id=\"board\" cellspacing=\"0\" cellpadding=\"0\">";
-	_.each(board.rows, function(row) {
+	_.each(this.rows, function (row) {
 		boardElement += "<tr>";
-		_.each(row, function(rowSquare) {
-			boardElement += "<td class=\"" + rowSquare.color + "\">";
+		_.each(row, function (rowSquare) {
+			boardElement += "<td id=\"" + rowSquare.htmlId() + "\" class=\"" + rowSquare.color + "\">";
 			if (typeof rowSquare.piece !== "undefined") {
 				boardElement += "<span class=\"" + rowSquare.piece.player.color + "\">&#x25C9;</span>";
 			}
@@ -65,9 +98,31 @@ board.draw = function() {
 	return boardElement;
 }
 
+board.initClickEvents = function () {
+	_.each(this.rows, function (row) {
+		_.each(row, function (rowSquare) {
+			$("#"+rowSquare.htmlId()).click(function () {
+				if (typeof rowSquare.piece !== "undefined") {
+					if (rowSquare.piece.player == currentTurn.player) {
+						currentTurn.selectSquare(rowSquare);
+					}
+				} else {
+					if (typeof currentTurn.selectedSquare !== "undefined") {
+						if (currentTurn.selectedSquare.canMoveTo(rowSquare) == true) {
+							rowSquare.piece = currentTurn.selectedSquare.piece;
+							currentTurn.selectedSquare.piece = undefined;
+							$('#board').replaceWith(board.draw());
+						}
+					}
+				}
+			});
+		});
+	});
+}
+
 // Initialize the pieces. (I'll clean this up later.)
 var pieces = [];
-_.times(3, function(i) {
+_.times(3, function (i) {
 	_.times(board.rows[i].length, function(ii) {
 		if (board.rows[i][ii].color == 'dark') {
 			board.rows[i][ii].piece = Object.create({});
@@ -85,7 +140,7 @@ _.times(3, function(i) {
 
 // Initialize the scoreboard.
 var scoreboard = { };
-scoreboard.draw = function() {
+scoreboard.draw = function () {
 	var scoreboardElement = "<table id=\"scoreboard\" cellspacing=\"1\" cellpadding=\"0\">";
 	scoreboardElement += "<tr>";
 	scoreboardElement += "<th>Player</th>";
