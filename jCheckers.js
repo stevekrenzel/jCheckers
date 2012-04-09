2d5895cf5a1eb39855891cfcf5188125be712016
Zack Parker <zack@zackparker.org>
2012-04-08 17:48:23 -0700

    Added a scoreboard.

--- jCheckers.js
+++ jCheckers.js
@@ -1,4 +1,5 @@
 // This I cribbed from http://javascript.crockford.com/prototypal.html, which is apparently how things work in this world.
+// And yeah, it seems dumb to be calling Object.create({}) all over the place. I'm assuming those'll end up being genuine classes.
 if (typeof Object.create !== 'function') {
 	Object.create = function (o) {
 		function F() {}
@@ -15,8 +16,16 @@ function reverseColor(thisColor) {
 	}
 }
 
+var player = {
+	piecesCount: function () {
+		_.filter(pieces, function (piece) {
+			piece.player == this;
+		}).length;
+	}
+}
+
 // Initialize the players.
-var players = [ Object.create({}), Object.create({}) ];
+var players = [ Object.create(player), Object.create(player) ];
 players[0].color = 'black';
 players[1].color = 'red';
 
@@ -41,7 +50,7 @@ board.draw = function() {
 		_.each(row, function(rowSquare) {
 			boardElement += "<td class=\"" + rowSquare.color + "\">";
 			if (typeof rowSquare.piece !== "undefined") {
-				boardElement += "<span class=\"piece_" + rowSquare.piece.player.color + "\">&#x25C9;</span>";
+				boardElement += "<span class=\"" + rowSquare.piece.player.color + "\">&#x25C9;</span>";
 			}
 			boardElement += "</td>";
 		});
@@ -52,16 +61,19 @@ board.draw = function() {
 }
 
 // Initialize the pieces. (I'll clean this up later.)
+var pieces = [];
 _.times(3, function(i) {
 	_.times(board.rows[i].length, function(ii) {
 		if (board.rows[i][ii].color == 'dark') {
 			board.rows[i][ii].piece = Object.create({});
 			board.rows[i][ii].piece.player = players[0];
+			pieces.push(board.rows[i][ii].piece);
 		}
 		var redmodifier = 5;
 		if (board.rows[i+redmodifier][ii].color == 'dark') {
 			board.rows[i+redmodifier][ii].piece = Object.create({});
 			board.rows[i+redmodifier][ii].piece.player = players[1];
+			pieces.push(board.rows[i+redmodifier][ii].piece);
 		}
 	});
 });
@@ -69,5 +81,20 @@ _.times(3, function(i) {
 // Initialize the scoreboard.
 var scoreboard = { };
 scoreboard.draw = function() {
-	return "";
+	var scoreboardElement = "<table id=\"scoreboard\" cellspacing=\"1\" cellpadding=\"0\">";
+	scoreboardElement += "<tr>";
+	scoreboardElement += "<th>Player</th>";
+	scoreboardElement += "<th>Pieces</th>";
+	scoreboardElement += "</tr>";
+	scoreboardElement += "<tr>";
+	scoreboardElement += "<td class=\"black\">Black</td>";
+	scoreboardElement += "<td>12</td>";
+	scoreboardElement += "</tr>";
+	scoreboardElement += "<tr>";
+	scoreboardElement += "<td class=\"red\">Red</td>";
+	scoreboardElement += "<td>12</td>";
+	scoreboardElement += "</tr>";
+	scoreboardElement += "</table>";
+
+	return scoreboardElement;
 }
